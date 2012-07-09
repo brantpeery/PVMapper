a6dbde5d18d0d214cc64fdc8dff6b8551b81604a
brantpeery_cp <SND\brantpeery_cp>
2012-07-09 19:40:06 +0000

    Added a new layout. Added in the source for OpenLayers so the I can look specics. Also updated the jQuery framework. Added a new MV called site details. Uses the new layout. Is a test with jQuery to control the OpenLayers content.

---
+++ Doe.PVMapper/Doe.PVMapper/Controllers/SiteDetailController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Doe.PVMapper.Controllers
+{
+    public class SiteDetailController : Controller
+    {
+        //
+        // GET: /SiteDetail/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Doe.PVMapper/Doe.PVMapper/Scripts/Tools/debug_log.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="../jquery-1.6.2.js" />
+
+var consoleDiv = $("<div id='log'></div>").appendTo('body');
+
+var debug = {
+    write: function (message) {
+        consoleDiv.append('<p>' + message + '</p>');
+        this.show();
+    },
+    flash: function () {
+        consoleDiv.show('normal', 'pulsate');
+    },
+    show: function () {
+        consoleDiv.show();
+    },
+    hide: function () {
+        consoleDiv.hide();
+    }
+}
