be9e0b6c2914374d3fca6b299323ddcf3cb31ef0
Matthew <mudnug@users.noreply.github.com>
2012-07-25 22:50:39 +0000

    Add: Site Area Tool

---
+++ Doe.PVMapper/Scripts/ViewScripts/SiteAreaTool.js
@@ -0,0 +1,32 @@
+﻿
+//view-source:http://openlayers.org/dev/examples/select-feature.html
+
+var sitesLayer = pvMapper.getSiteLayer();
+if (sitesLayer) {
+    sitesLayer.events.on({
+        'featureselected': function (feature) {
+            console.log(this.selectedFeatures.length);
+        },
+        'featureunselected': function (feature) {
+            console.log(this.selectedFeatures.length);
+        }
+    });
+
+    var select = new OpenLayers.Control.SelectFeature(
+        sitesLayer,
+        {
+            clickout: false, toggle: false,
+            multiple: false, hover: false,
+            toggleKey: "ctrlKey", // ctrl key removes from selection
+            multipleKey: "shiftKey", // shift key adds to selection
+            box: true
+        });
+
+    pvMapper.map.addControl(select);
+    select.activate();
+
+    //get selected site.
+
+    //todo: calculate area.
+    pvMapper.postScore( "High", Math.random(), "SiteName", "SiteAreaTool");
+}
