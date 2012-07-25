46b0db52a9f9a5756bf1222a580e2a962b7dbdf3
brantpeery_cp <brant@atechinnovations.com>
2012-07-25 18:54:17 +0000

    UPDATE: Add Site button lets the user draw a site then set metadata. Displays information about the site but doesn't yet save it.

--- Doe.PVMapper/Scripts/ViewScripts/AddNewSite.js
+++ Doe.PVMapper/Scripts/ViewScripts/AddNewSite.js
@@ -4,39 +4,114 @@ Add site plugin
 Contributors: Brant Peery, Matthew Klien
 */
 
+var tools=[];
 
 pvMapper.onReady(function () {
 
-    var addSite = new Ext.Action({
+    var addSiteTool = new Ext.Action({
         text: "Add Site",
         handler: function () {
+            var tool = new addSite(pvMapper.map);
 
-            var pointLayer = new OpenLayers.Layer.Vector("Point Layer");
-            pointLayer.id = "Hola";
+            tools.push(tool);
 
-            pvMapper.map.addLayer(pointLayer);
-            var tool = new OpenLayers.Control.DrawFeature(pointLayer, OpenLayers.Handler.Point);
-            pvMapper.map.addControl(tool);
-            tool.activate();
 
-            //var layer = pvMapper.map.getLayer("Hola");
-            //layer.features[0].geometry[0].x;
-            // view-source:http://openlayers.org/dev/examples/click.html
+            
         }
 
     });
-    pvMapper.toolbar.add(addSite);
+    pvMapper.toolbar.add(addSiteTool);
 });
 
 //The main plugin object. Conforms to the plugin definition set by the framework
 function addSite(map, layer) {
-    var self = this; //Makes the 'this' object accessable from private methods
+    var self = this; //Makes the 'this' object accessable from the private methods
     var WKT;
     var currentSiteName;
+    var feature;
+    var wiz;
     this.layer = createSiteLayer(map);
     this.mapControl = new OpenLayers.Control.DrawFeature(this.layer, OpenLayers.Handler.Polygon);
+    map.addControl(this.mapControl);
 
-    activateDrawSite(); //Turn the tool on 
+    activateDrawSite();
+    this.mapControl.events.register("featureadded", this.mapControl, function (data) {
+        var control = this;
+        feature = data.feature;
+        
+        //Give a way to cancel the creation
+
+        //This is where a save to the database might happen
+        WKT = feature.geometry.toString();
+
+        var kml = new OpenLayers.Format.KML();
+        //alert(kml.write(feature));
+        //Then continue to collect the needed form data
+        ///HACK: This needs to use the framework standard way of doing it. For now I am going to assume that I have access to EXTjs 3
+        wiz = new Ext.Window({
+            layout:'form',
+            modal:true,
+            collapsible: true,
+            id: "siteWizard",
+            frame: true,
+            title: "Create a New Site",
+            bodyPadding: '5 5 0',
+            width: 350,
+            defaultType: 'textfield',
+            items: [{
+                fieldLabel: 'Site Name',
+                hideLabel:false,
+                name: 'name',
+                id:'name'
+            }, {
+                fieldLabel: 'Site Description',
+                xtype:'textarea',
+                name: 'siteDescription',
+                id:'sitedescription'
+            }],
+
+            buttons: [{
+                text: 'Save',
+                handler: function (b, e) {
+                    var name = Ext.getCmp("name").getValue();
+                    var desc = Ext.getCmp("sitedescription").getValue();
+
+                    feature.id=name;
+                    feature.name=name;
+                    wiz.destroy();
+
+                    var msg = "The feature has been named " + name + " and it is described as " + desc + ". \n " + WKT;
+                    $msgdiv = $('<div>' + msg + '</div>').appendTo('body').css({ 'position': 'fixed', top: '0', left: 0, width: "100%", background: 'red', color: 'white' });
+                    $msgdiv.show();
+                    $msgdiv.fadeOut(10000);
+
+                    //alert (msg);
+                }
+            }, {
+                text: 'Cancel',
+                handler: function (b, e) {
+                    feature.destroy();
+                    control.cancel();
+                    wiz.destroy();
+                }
+            }]
+     
+        })
+
+        wiz.show();
+        
+
+
+
+        //Now save the whole thing
+    });
+    
+    function handleSave(b, e) {
+        var msg;
+        
+
+        alert(feature.geometry.toString());
+    }
 
     function createLayer () { }
     function activateDrawSite() {
@@ -49,7 +124,7 @@ function addSite(map, layer) {
 
     function createAddSiteDialog() { }
     function createSiteLayer(map) {
-        sitelayer = new OpenLayers.Layer.Vector("Sites");
+        var sitelayer = new OpenLayers.Layer.Vector("Sites");
         sitelayer.id = "SiteLayer";
         map.addLayer(sitelayer);
         return sitelayer;
