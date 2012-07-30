ad63812d0c8dcdc5a7d299484009bb4552ad7f9b
mudnug_cp <SND\mudnug_cp>
2012-07-30 22:40:28 +0000

    Add: create site layer during page load

--- Doe.PVMapper/Doe.PVMapper/Scripts/ViewScripts/LoadProjectSites.js
+++ Doe.PVMapper/Doe.PVMapper/Scripts/ViewScripts/LoadProjectSites.js
@@ -1,15 +1,14 @@
 ﻿Ext.onReady(function () {
-  
-     $.get("/api/ProjectSite/")
-        .done(function( sites ) { 
-            for (var i = 0; i < sites.length; i++)
-            {
-    // api/ProjectSite/ will return ids of sites. This seems to be a bug.
-               pvMapper.getSite(sites[i].id)
-                 .done(function (site) {
-                       // todo: load users's site when loading the map.
-                 });
-            }
-    });
- 
+
+    // todo: load only users's site when loading the map.
+    $.get("/api/ProjectSite/")
+
+        .done(function (sites) {
+            //for (var i = 0; i < sites.length; i++) {
+            //    var poly = new OpenLayers.Format.WKT().read(sites[i].polygonGeometry);
+            //    poly.fid = sites[i].siteId;
+            //    polyLayer.addFeatures([poly]);
+            //}
+        });
+
 });
