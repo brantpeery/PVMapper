49c7533493cf5d3292418811cb0ddcbd1059f0eb
Matthew <mudnug@users.noreply.github.com>
2012-08-10 00:20:21 +0000

    Add: emit metadata with score information

--- Doe.PVMapper/Scripts/ViewScripts/Scoreboard.js
+++ Doe.PVMapper/Scripts/ViewScripts/Scoreboard.js
@@ -1,20 +1,66 @@
 ﻿pvMapper.onReady(function () {
+    var grid;
 
-    // Define Company entity
-    // Null out built in convert functions for performance *because the raw data is known to be valid*
-    // Specifying defaultValue as undefined will also save code. *As long as there will always be values in the data, or the app tolerates undefined field values*
-    Ext.define('Company', {
+    Ext.define('EmptyModel', {
         extend: 'Ext.data.Model',
         fields: [
-           { name: 'company', type: 'string' },
-           { name: 'price', type: 'float', convert: null, defaultValue: undefined },
-           { name: 'change', type: 'float', convert: null, defaultValue: undefined },
+           // Null out built in convert functions for performance *because the raw data is known to be valid*
+           // Specifying defaultValue as undefined will also save code. *As long as there will always be values in the data, or the app tolerates undefined field values*
+           //{ name: 'company', type: 'string' },
+           //{ name: 'price', type: 'float', convert: null, defaultValue: undefined },
+           //{ name: 'change', type: 'float', convert: null, defaultValue: undefined },
         ],
-        idProperty: 'company'
     });
 
-    // sample static data for the store
-    var myData = [["3m co.", 32.3, 12.2, 23.1], ["Alcoa Inc", 32.3, 12.2, 23.1], ["Altria Group Inc", 32.3, 12.2, 23.1], ["American Express Company", 32.3, 12.2, 23.1]];
+    //columns: [
+    //    {
+    //        text: 'Tool',
+    //        flex: 1,
+    //        sortable: true,
+    //        dataIndex: 'company'
+    //    },
+    //    {
+    //        text: 'Site 1',
+    //        width: 75,
+    //        sortable: true,
+    //        dataIndex: 'price'
+    //    },
+    //    {
+    //        text: 'Site 2',
+    //        width: 75,
+    //        sortable: true,
+    //        renderer: change,
+    //        dataIndex: 'price'
+    //    },
+    //    {
+    //        text: 'Site 3',
+    //        width: 75,
+    //        sortable: true,
+    //        renderer: change,
+    //        dataIndex: 'price'
+    //    },
+    //    {
+    //        text: 'Site 4',
+    //        width: 75,
+    //        sortable: true,
+    //        renderer: change,
+    //        dataIndex: 'change'
+    //    },
+    //    {
+    //        menuDisabled: true,
+    //        sortable: false,
+    //        xtype: 'actioncolumn',
+    //        width: 50,
+    //        items: [{
+    //            // icon: '../shared/icons/fam/icon.gif',  // Use a URL in the icon config
+    //            tooltip: 'Sell stock',
+    //            handler: function (grid, rowIndex, colIndex) {
+    //                var rec = store.getAt(rowIndex);
+    //                alert("Running " + rec.get('name'));
+    //            }
+    //        }]
+    //    }
+    //],
 
     /**
      * Custom function used for column renderer
@@ -31,76 +77,34 @@
 
     // create the data store
     var store = Ext.create('Ext.data.ArrayStore', {
-        model: 'Company',
-        // data: myData
+        model: 'EmptyModel',
         proxy: {
             type: 'ajax',
             url: '/api/Scoreboard',
-            reader: 'array'
+            //url: 'json.txt',
+            reader: 'json'
         },
-        autoLoad: true
+        autoLoad: true,
+        listeners: {
+            'metachange': function (store, meta) {
+                grid.reconfigure(store, meta.columns);
+            }
+        }
 
     });
 
     // create the Grid
-    var grid = Ext.create('Ext.grid.Panel', {
+    grid = Ext.create('Ext.grid.Panel', {
         store: store,
-        stateful: true,
+        //stateful: true,
         multiSelect: true,
-        stateId: 'stateGrid',
-        columns: [
-            {
-                text: 'Tool',
-                flex: 1,
-                sortable: true,
-                dataIndex: 'company'
-            },
-            {
-                text: 'Site 1',
-                width: 75,
-                sortable: true,
-                dataIndex: 'price'
-            },
-            {
-                text: 'Site 2',
-                width: 75,
-                sortable: true,
-                renderer: change,
-                dataIndex: 'price'
-            },
-            {
-                text: 'Site 3',
-                width: 75,
-                sortable: true,
-                renderer: change,
-                dataIndex: 'price'
-            },
-            {
-                text: 'Site 4',
-                width: 75,
-                sortable: true,
-                renderer: change,
-                dataIndex: 'change'
-            },
-            {
-                menuDisabled: true,
-                sortable: false,
-                xtype: 'actioncolumn',
-                width: 50,
-                items: [{
-                    // icon: '../shared/icons/fam/icon.gif',  // Use a URL in the icon config
-                    tooltip: 'Sell stock',
-                    handler: function (grid, rowIndex, colIndex) {
-                        var rec = store.getAt(rowIndex);
-                        alert("Running " + rec.get('name'));
-                    }
-                }]
-            }
-        ],
-
+        //stateId: 'stateGrid',
+        columns: [{
+            header: 'Loading ...'
+        }],     
         viewConfig: {
             stripeRows: true,
-            enableTextSelection: true
+            // enableTextSelection: true
         }
     });
 
@@ -116,4 +120,7 @@
             }
         }
     });
+
+    // Activate the Scoreboard by default.
+    pvMapper.tabs.setActiveTab(1);
 });
