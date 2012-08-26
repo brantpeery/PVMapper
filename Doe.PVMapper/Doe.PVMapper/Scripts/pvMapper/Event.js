72676756bd6b6810724f39ee51c50546b066940f
brantpeery_cp <SND\brantpeery_cp>
2012-08-26 08:22:43 +0000

    Updating the pvMapper API (WIP)

---
+++ Doe.PVMapper/Doe.PVMapper/Scripts/pvMapper/Event.js
@@ -0,0 +1,35 @@
+﻿//Event manager
+//Parameter: <boolean> allowDuplicateHandler 
+//          true will allow the registration of the same function multiple times as a callback
+//          false will only allow a function to be registered once as a callback. It will ignore any other attempts to register the same function.
+
+//Use the managager by creating a new event in code. 
+//  ex. this.myEvent = new Event();
+//Then attach the handlers to the event using the addHandler method
+
+
+function Event(allowDuplicateHandler) {
+    this.eventHandlers = new Array();
+    this.allowDuplicateHandler = false || allowDuplicateHandler;
+}
+
+Event.prototype.addHandler = function (callBack) {
+    if (this.eventHandlers.indexOf(callBack) == -1 || this.allowDuplicateHandler) {
+        this.eventHandlers.push(callBack);
+    }
+}
+Event.prototype.removeHandler = function (handler) {
+    var idx;
+    while (this.eventHandlers) {
+        idx = this.eventHandlers.indexOf(handler)
+        if (idx == -1) { break; }
+        this.eventHandlers.splice(idx, 1);
+    }
+}
+Event.prototype.fire = function (context, eventArgs) {
+    var self = this;
+    $.each(this.eventHandlers, function () {
+        this.apply(context, eventArgs);
+    });
+}
+Event.prototype.getBeans = function () { console.log(beans); }
--- Doe.PVMapper/Doe.PVMapper/Scripts/pvMapper/scoreboard.js
+++ Doe.PVMapper/Doe.PVMapper/Scripts/pvMapper/scoreboard.js
@@ -1,1 +1,15 @@
 ﻿
+function scoreBoard() {
+    this.scoreChanged = new Event();
+    this.scoresInvalidated = new Event();
+
+    this.addLine;
+    this.removeLine;
+    this.scoreLines;
+
+    function onScoreChanged() {
+        //let all the other modules that care know that a score changed
+
+    };
+
+}
