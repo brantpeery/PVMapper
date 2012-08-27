4150e2a46369d5b5157d55dab52d3e2f633667dc
brantpeery_cp <SND\brantpeery_cp>
2012-08-27 06:08:16 +0000

    pvMapper API elements added

--- Doe.PVMapper/Doe.PVMapper/Scripts/pvMapper/scoreline.js
+++ Doe.PVMapper/Doe.PVMapper/Scripts/pvMapper/scoreline.js
@@ -1,39 +1,44 @@
 ﻿/// <reference path="../_references.js" />
 
-function scoreLine(scoreboard, name, updateScoreCallBack) {
-    //Make sure we have the correct parameters 
-    scoreboard = (scoreboard.prototype == scoreboard) ? scoreboard : pvMapper.scoreBoard;
+(function (pvM){
+    pvM.scoreLine = function (scoreboard, name, updateScoreCallBack) {
+        this.scoreChangeEvent = new Event();
+        this.updatingScoresEvent = new Event();
 
-    //name = (name) ? name : "Module scoring tool" + scoreboard.scoreLines.count;
-    updateScoreCallBack = ($.isFunction(updateScoreCallBack)) ? updateScoreCallBack : null;
+        //Make sure we have the correct parameters 
+        scoreboard = (scoreboard.prototype == scoreboard) ? scoreboard : pvMapper.scoreBoard;
 
-    var scoreboard = scoreboard; //The scoreboard that the line will belong to
+        //name = (name) ? name : "Module scoring tool" + scoreboard.scoreLines.count;
+        updateScoreCallBack = ($.isFunction(updateScoreCallBack)) ? updateScoreCallBack : null;
 
-    this.name; //The name that will show up in the row
-    this.description; //The popup information that will show up on mouse hover
-    this.scores; //A collection of scores that store all the information for the colums of this line
-    this.updateScoreCallback = updateScoreCallBack;
+        var scoreboard = scoreboard; //The scoreboard that the line will belong to
 
-    //Observes the siteChanged event for the sites that this line cares about 
-    this.onSiteChanged = function (event) {
-        //Pass the event on to the module that created the line
-        //change the context to this line
-        this.updateScoreCallback.call(this, event.site);
+        this.name; //The name that will show up in the row
+        this.description; //The popup information that will show up on mouse hover
+        this.scores; //A collection of scores that store all the information for the colums of this line
+        this.updateScoreCallback = updateScoreCallBack;
+
+        //Observes the siteChanged event for the sites that this line cares about 
+        this.onSiteChanged = function (event) {
+            //Pass the event on to the module that created the line
+            //change the context to this line
+            this.updateScoreCallback.call(this, event.site);
+        }
+
+        //Access to private members
+        //Give read only access to the scoreboard object 
+        this.getScoreBoard = function () { return scoreboard };
     }
 
-    //Access to private members
-    //Give read only access to the scoreboard object 
-    this.getScoreBoard = function () { return scoreboard };
-}
+    pvM.scoreLine.prototype.setScore = function (featureId, options) {
+        //use jQuery to merge the options object into the score object
+    };
 
-scoreLine.prototype.setScore = function (featureId, options) {
-    //use jQuery to merge the options object into the score object
-};
+    //Used to force the module to update all the scores for this line 
+    pvM.scoreLine.prototype.updateScores = function () {
+        for (idx = 0; idx > this.scores.length - 1; idx++) {
 
-//Used to force the module to update all the scores for this line 
-scoreLine.prototype.updateScores = function () {
-    for (idx = 0; idx > this.scores.length - 1; idx++) {
-        
-        this.updateScoreCallback.call(this, this.scores[idx].site)
+            this.updateScoreCallback.call(this, this.scores[idx].site)
+        }
     }
-}
+})(pvMapper);
