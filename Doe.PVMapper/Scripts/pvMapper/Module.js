79af2387e0cb204f94eb8faecae13ee46eb27a09
brantpeery_cp <brant@atechinnovations.com>
2012-08-31 11:59:53 +0000

    Rename files to title case to match the containing class Create the Module.js file for managing modules.

---
+++ Doe.PVMapper/Scripts/pvMapper/Module.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="../_references.js" />
+
+//Creates and registers a custom modual's attributes, tools and intents. 
+(function (pvM) {
+    //This is a factory     
+    pvM.Module = function (settings) {
+        this.id = "";
+        this.author = "";
+        this.version = "";
+
+        this.init = function () { };          //Called when the tool is loaded as a module.
+        this.destroy = function () { };       //Called when the tool needs to completely remove itself from the interface and object tree
+        this.activate = function () { };      //Called when the tool is checkmarked or activated by the system or user
+        this.deactivate = function () { };    //Called when the tool is unchecked or deactivated by the system or user
+
+        this.scoreboardLineItems      
+
+    }
+
+
+    pvM.registerModule();
+    pvM.registerSiteTool();
+    pvM.register
+
+
+})(pvMapper);
---
+++ Doe.PVMapper/Scripts/pvMapper/Site.js
@@ -0,0 +1,42 @@
+﻿/// <reference path="../_references.js" />
+
+//Site object 
+(function (pvM) {
+    pvM.site = function (/*OpenLayers.Feature*/ feature) {
+        //Check the parameters
+        if ($(feature).isPrototypeOf(OpenLayers.Feature)) { throw ('The parameter "feature" must be an OpenLayers.Feature'); }
+
+        this.id = feature.fid;
+        this.feature = feature;
+        this.feature.site = this;
+        this.geometry = feature.Geometry;
+        this.name = feature.attributes.name;
+        this.description = feature.attributes.description;
+        this.popupHTML;
+
+        this.onFeatureSelected = function (event) {
+            this.selectEvent.fire(event);
+        };
+        this.onFeatureChanged = function (event) {
+            this.changeEvent.fire(event);
+        };
+        this.select = function () {
+            //Deselect the currently selected feature
+            //Change the selected feature in the pvM object
+            //Change the state of this feature to selected
+        };
+
+        this.changeEvent = new Event();
+        this.createEvent = new Event();
+        this.destroyEvent = new Event();
+        this.labelChangeEvent = new Event();
+        this.selectEvent = new Event();
+        this.unselectEvent = new Event();
+
+
+
+
+    }
+
+})(pvMapper);
+
