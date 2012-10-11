db6821fbba7cc18c46c8692da020c88bfb2a50a2
LengVang_cp <SND\LengVang_cp>
2012-10-11 14:44:29 +0000

    new graph algorithm

--- Doe.PVMapper/Doe.PVMapper/Scripts/UI/view/weightFunctions/FunctionView.js
+++ Doe.PVMapper/Doe.PVMapper/Scripts/UI/view/weightFunctions/FunctionView.js
@@ -8,46 +8,53 @@
 //#region function graph
 var board, f2;
 function loadBoard() {
-  JXG.Options.ticks.majorHeight = 20;
-  JXG.Options.ticks.minorTicks = 5;
-  JXG.Options.ticks.insertTicks = false;
-  JXG.Options.ticks.minorHeight = 5;
-  JXG.Options.ticks.ticksDistance = 10;
   board = JXG.JSXGraph.initBoard( 'FunctionBox-body', { boundingbox: [0, 1.05, 100, -.05], axis: true, showCopyright: false, showNavigation: false } );
   f2 = board.create( 'functiongraph', [UtilityFunctions.utilityFunction1], { strokeWidth: 3, strokeColor: "red" } );
 };
 
 function updateBoard() {
   var target = Ext.getCmp( 'function-target' );
-  board.setBoundingBox([target.min-target.minValue*.1, 1.1, target.maxValue *1.1, -.1]);
+  f2.Y = ( Ext.getCmp( 'function-mode' ).value == 'More is better' ) ? UtilityFunctions.utilityFunction1 : UtilityFunctions.utilityFunction2;
+
+  if ( target.maxValue - target.minValue != 0 ) {
+    var diff = target.maxValue - target.minValue;
+    board.setBoundingBox( [target.minValue - diff * .1, 1.1, target.maxValue + diff * .1, -.1] );
+    board.update();
+  }
 }
 
 var UtilityFunctions = {
   utilityFunction1: function ( x ) {
-    var l = parseInt( $( "#target-MinValue-inputEl" ).val() );
-    var b = parseInt( $( "#function-target-inputEl" ).val() );
-    var h = parseInt( $( "#target-MaxValue-inputEl" ).val() );
-    var s = parseInt( $( "#function-slope-inputEl" ).val() );
-    var y = 0;
+    //var l = parseInt( $( "#target-MinValue-inputEl" ).val() );
+    //var b = parseInt( $( "#function-target-inputEl" ).val() );
+    //var h = parseInt( $( "#target-MaxValue-inputEl" ).val() );
+    //var sRatio = ( parseInt( $( "#function-slope-inputEl" ).val() ) / 5 ) + .3;
+    //var y = 0;
 
-    if ( x >= h ) y = 1;
-    else if ( x <= l ) y = 0;
-    else y = 1 / ( 1 + Math.pow(( b - l ) / ( x - l ), ( 2 * ( 1 / s ) * ( b + x - 2 * l ) ) ) );
+    //var s = Math.max( 2 / ( b - l ), 2 / ( h - b ) );
+    ////s = s * ( sRatio );
 
-    if ( y >= 1 ) y = 1;
-    if ( y <= 0 ) y = 0;
-    return 1 - y;
+    //if ( x >= h ) y = 1;
+    //else if ( x <= l ) y = 0;
+    //else y = 1 / ( 1 + Math.pow(( b - l ) / ( x - l ), ( 2 * ( 1 / s ) * ( b + x - 2 * l ) ) ) );
+
+    //if ( y >= 1 ) y = 1;
+    //if ( y <= 0 ) y = 0;
+    return 1 - UtilityFunctions.utilityFunction2( x );
   },
 
   utilityFunction2: function ( x ) {
     var l = parseInt( $( "#target-MinValue-inputEl" ).val() );
     var b = parseInt( $( "#function-target-inputEl" ).val() );
     var h = parseInt( $( "#target-MaxValue-inputEl" ).val() );
-    var s = 1 / parseInt( $( "#function-slope-inputEl" ).val() );
+    var sRatio = ( parseInt( $( "#function-slope-inputEl" ).val() ) / 5 ) + .3;
     var y = 0;
 
-    if ( x >= h ) y = 1;
-    else if ( x <= l ) y = 0;
+    var s = Math.min( -2 / ( b - l ), -2 / ( h - b ) );
+    s = s * ( sRatio );
+
+    if ( x >= h ) y = 0;
+    else if ( x <= l ) y = 1;
     else y = ( x < b ) ? 1 / ( 1 + Math.pow(( b - l ) / ( x - l ), ( 2 * s * ( b + x - 2 * l ) ) ) ) :
         1 - ( 1 / ( 1 + Math.pow(( b - ( 2 * b - h ) ) / ( ( 2 * b - x ) - ( 2 * b - h ) ), ( 2 * s * ( b + ( 2 * b - x ) - 2 * ( 2 * b - h ) ) ) ) ) );
     if ( y >= 1 ) y = 1;
