a782a3d17ebce40746808f4e78c80f03c582444e
LengVang_cp <SND\LengVang_cp>
2013-01-21 22:41:28 +0000

    Updated TypeScript files for the API. Mostly done. Need to convert DataManager and PVMapper main events. Non of the new code has been unit tested or code reviewed.

--- Doe.PVMapper/Doe.PVMapper/Scripts/pvMapper/TSMapper/Score.js
+++ Doe.PVMapper/Doe.PVMapper/Scripts/pvMapper/TSMapper/Score.js
@@ -9,9 +9,20 @@ var pvMapper;
         pvM.displayMessage(value);
     }
     pvMapper.displayMessage = displayMessage;
-    // Class
+    /***
+    * A PVMapper.Score object. Tracks the score for a site. Ties a site to a scoring line and represents a line's value cell for a site.
+    *
+    * @variable {string} myvalue The value that was calculated for the site. Uses the site geometry and the tool to figure the value. Updated by the tool
+    * @variable {string} popupMessage The message to display when the mouse hovers  over the scoring cell on the interface
+    */
     var Score = (function () {
-        // Constructor
+        /**
+        * Creates a Score object. Ties the site's change event to this scores score changed event
+        *
+        * @constructor
+        * @param {PVMapper.Site}  site The site that this score will track
+        * @return {PVMapper.Score} New Score object
+        */
         function Score(site) {
             this.valueChangeEvent = new pvMapper.Event();
             this.invalidateEvent = new pvMapper.Event();
@@ -28,7 +39,8 @@ var pvMapper;
             //Grab onto the change event for the site
             this.site.changeEvent.addHandler(this.onSiteChanged);
         }
-        Score.prototype.calculateUtility = //Calculates the utility score for the value passed in or if no value is passed in it uses the current value property
+        /// <Summary>A reference the this object independent of scope</Summary>
+                Score.prototype.calculateUtility = //Calculates the utility score for the value passed in or if no value is passed in it uses the current value property
         function (value) {
             if(typeof (value) != 'undefined') {
                 return this.updateValue(value);
