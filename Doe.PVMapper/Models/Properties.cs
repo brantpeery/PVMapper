7e771aa19098c6907cf9e9cb9ff5620532bbe623
brantpeery_cp <brant@atechinnovations.com>
2013-02-12 17:20:07 +0000

    Updated the references to MongoLibrary.dll

--- Doe.PVMapper/Models/Properties.cs
+++ Doe.PVMapper/Models/Properties.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
-using DreamSongs.MongoRepository;
+using MongoRepository;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using Newtonsoft.Json;
--- Doe.PVMapper/Models/SiteScore.cs
+++ Doe.PVMapper/Models/SiteScore.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using DreamSongs.MongoRepository;
+using MongoRepository;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using Newtonsoft.Json;
