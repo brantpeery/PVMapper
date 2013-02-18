f4afdcf007ff6fa99a8e443f613097fa07f7f055
brantpeery_cp <brant@atechinnovations.com>
2013-02-18 17:00:11 +0000

    Create the siteManager unit tests and fix bugs that it found.

--- Doe.PVMapper/Scripts/pvMapper/TSMapper/SiteManager.ts
+++ Doe.PVMapper/Scripts/pvMapper/TSMapper/SiteManager.ts
@@ -4,8 +4,8 @@
 module pvMapper {
     //import pvM = pvMapper;
 
-    export var readyEvent = new Event(false);
-    export function onReady(fn: () =>void ) {
+    export var readyEvent = new Event(false); //Belongs in the PVMapper class
+    export function onReady(fn: () => void ) {
         this.readyEvent.addHandler(fn);
         return pvMapper;
     }
@@ -14,41 +14,44 @@ module pvMapper {
         public siteAdded: pvMapper.Event = new pvMapper.Event();
         public siteRemoved: pvMapper.Event = new pvMapper.Event();
 
-        sites: Site[] = [];
+        private sites: Site[] = [];
         public getSites() {
             return this.sites;
         }
-        public getSite(index: string): Site;
-        public getSite(index: number): Site;
-        public getSite(index: any):Site {
+        //public getSite(index: string): Site;
+        //public getSite(index: any): Site {
+        public getSite(index: number): Site {
             return this.sites[index];
         }
-        public addSite(site: any);
-        public addSite(feature: OpenLayers.SiteFeature) {
-          var aSite = new Site(feature);
-          this.sites.push(aSite);
-          this.siteAdded.fire(aSite, feature);
-        }
-         
-/*       public addSite(site: Site) {
-           if (site instanceof (OpenLayers.Feature)) { //Convert the feature to a site
-                site = new Site(site);
-            } else if (site instanceof Options) {
 
-            } else if (!site instanceof Site) { //If the site was not one of the above and also is not a site then error
-                console.log("Cannot create a site from a type : " + typeof (site) + " Site not created.");
-            }
+        public addSite(site: pvMapper.Site) {
             this.sites.push(site);
-            this.siteAdded.fire(site, [{ site: site }, site]);
-        }
-*/
-        public removeSite(site) {
         }
 
-        //Handles the change event for the features on the sitelayer. Will fire the sites change event if the 
-        //  feature that changed is a project site
-        //@Parameter event {OpenLayers.Event object with a feature property that is a reference to the feature that changed
-        //@See http://dev.openlayers.org/apidocs/files/OpenLayers/Layer/Vector-js.html#OpenLayers.Layer.Vector.events
+        public createSite(feature: OpenLayers.SiteFeature) {
+            var aSite = new Site(feature);
+            this.sites.push(aSite);
+            this.siteAdded.fire(aSite, feature);
+        }
+
+        /**
+        Removes a site from the sites array.
+        */
+        public removeSite(site) {
+            //find the site
+            var idx: number = this.sites.lastIndexOf(site);
+            if (idx !== -1) {
+                this.sites.splice(idx, 1);
+            }
+        }
+
+
+        /**
+        handles the change event for the features on the sitelayer. will fire the sites change event if the 
+          feature that changed is a project site
+        @parameter event {openlayers.event object with a feature property that is a reference to the feature that changed
+        @See http://dev.openlayers.org/apidocs/files/OpenLayers/Layer/Vector-js.html#OpenLayers.Layer.Vector.events
+        */
         public featureChangedHandler(event: any) {
             console.log("Feature change detected by the site manager");
             if (event.feature && event.feature.site) {
@@ -59,10 +62,25 @@ module pvMapper {
                     console.log("An error occurred while trying to fire the feature change event for a site from the site manager");
                     console.error(e);
                 }
+            } else {
+                console.log("The feature was not a site");
             }
         }
     }
-   //instantiate siteManager object.
-   export var siteManager: SiteManager = new SiteManager();
-
+    //instantiate siteManager object.
+    export var siteManager: SiteManager = new SiteManager();
 }
+
+/*       public addSite(site: Site) {
+if (site instanceof (OpenLayers.Feature)) { //Convert the feature to a site
+    site = new Site(site);
+} else if (site instanceof Options) {
+
+} else if (!site instanceof Site) { //If the site was not one of the above and also is not a site then error
+    console.log("Cannot create a site from a type : " + typeof (site) + " Site not created.");
+}
+this.sites.push(site);
+this.siteAdded.fire(site, [{ site: site }, site]);
+}
+*/
+
