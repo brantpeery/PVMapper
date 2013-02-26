61e112588e4a850b7f8ef19bd13bb6e096af266b
LengVang_cp <SND\LengVang_cp>
2013-02-26 23:16:53 +0000

    created

---
+++ Doe.PVMapper-Dev/Doe.PVMapper/Scripts/pvMapper/TSMapper/LandUseModule.ts
@@ -0,0 +1,89 @@
+/// <reference path="pvMapper.ts" />
+/// <reference path="Site.ts" />
+/// <reference path="Score.ts" />
+/// <reference path="Tools.ts" />
+/// <reference path="Options.d.ts" />
+/// <reference path="Module.ts" />
+
+
+module INLModules {
+  export class LandUseModule {
+    constructor() {
+
+      var myModule: pvMapper.Module = new pvMapper.Module({
+        id: "LandUseModule",
+        author: "Leng Vang, INL",
+        version: "0.1.ts",
+
+        activate: () => {
+          addLandUseMap();
+        },
+        deactivate: () => {
+          removeLandUseMap();
+        },
+        destroy: null,
+        init: null,
+
+        scoringTools: [{
+          activate: null,
+          deactivate: null,
+          destroy: null,
+          init: null,
+
+          title: "Land Uses",
+          description: "Display land use boundaries.",
+          onScoreAdded: (e, score: pvMapper.Score) => {
+          },
+          onSiteChange: function (e, s) {
+            ///////////////////////////////////////////getFeatureInfo(s.site);
+            s.updateValue("Land score");
+          },
+          calculateValueCallback: (site: pvMapper.Site): number => {
+            ///////////////////////////////////////////getFeatureInfo(site);
+            return -1;
+          },
+        }],
+
+        infoTools: null
+      });
+    }
+
+  }
+}
+
+var landUseInstance = new INLModules.LandUseModule();
+var landUseUrl = "http://dingo.gapanalysisprogram.com/ArcGIS/services/PADUS/PADUS_owner/MapServer/WMSServer?";
+var landUseLayer;
+
+function addLandUseMap() {
+  var landBounds = new OpenLayers.Bounds(-20037508, -20037508, 20037508, 20037508.34);
+
+  landUseLayer = new OpenLayers.Layer.WMS(
+          "Land Use",
+          landUseUrl,
+          {
+            maxExtent: landBounds,
+            layers: "0",
+            srs: "EPSG:102113",
+            layer_type: "Feature Layer",
+            transparent: "true",
+            format: "image/gif",
+            exceptions: "application/vnd.ogc.se_inimage",
+            maxResolution: 156543.0339
+          },
+          { isBaseLayer: false }
+          );
+  landUseLayer.arcGisEpsgOverride = true;
+  landUseLayer.setOpacity(0.3);
+  pvMapper.map.addLayer(landUseLayer);
+}
+
+function removeLandUseMap() {
+  pvMapper.map.removeLayer(landUseLayer, false);
+}
+
+function getLandUseLayerInfo() {
+
+}
+
+
