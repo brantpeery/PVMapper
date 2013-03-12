3ff4049a6a8febd945ab36a0c44ae89420662619
scottbbrown_cp <scottbrown@inl.gov>
2013-03-12 22:05:30 +0000

    Removed all debugging .map files from the repository. Some of us are building these, and some of us aren't. At best they're a nuisance. At worst they're a liability for AppHarbor building the Release branch given that there will likely be outdated and/or missing .map files. Let's keep these files local. Also, somebody missed committing Event.js and Event.min.js to the repository.

---
+++ Doe.PVMapper/Scripts/pvMapper/TSMapper/Event.js
@@ -0,0 +1,48 @@
+var pvMapper;
+(function (pvMapper) {
+    /*
+    Is a publish point. Uses the handlers and fire method to publish events
+    */
+    var Event = (function () {
+        /// Creates the publish point.
+        /// allowDuplicateHandler if set to true will allow the same function to subscribe more than once.
+        function Event(allowDuplicateHandler) {
+            if (typeof allowDuplicateHandler === "undefined") { allowDuplicateHandler = false; }
+            this.allowDuplicateHandler = allowDuplicateHandler;
+            //for event parameter data tag
+            this.data = null;
+            this.eventHandlers = new Array();
+        }
+        Event.prototype.addHandler = ///
+        function (callBack) {
+            if(this.eventHandlers.indexOf(callBack) == -1 || this.allowDuplicateHandler) {
+                this.eventHandlers.push(callBack);
+            }
+        };
+        Event.prototype.removeHandler = function (handler) {
+            var idx;
+            while(this.eventHandlers) {
+                idx = this.eventHandlers.indexOf(handler);
+                if(idx == -1) {
+                    break;
+                }
+                this.eventHandlers.splice(idx, 1);
+            }
+        };
+        Event.prototype.fire = function (context, eventArgs) {
+            var self = this;
+            if(!(eventArgs instanceof Array)) {
+                eventArgs = [
+                    eventArgs
+                ];
+            }
+            self.eventHandlers.map(function (func, idx) {
+                if(typeof (func) != 'undefined') {
+                    func.apply(context, eventArgs);
+                }
+            });
+        };
+        return Event;
+    })();
+    pvMapper.Event = Event;    
+})(pvMapper || (pvMapper = {}));
