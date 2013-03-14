fec12930af9407727438c61d82244515c86ac02b
scottbbrown_cp <scottbrown@inl.gov>
2013-03-14 01:14:26 +0000

    Merging demo DEM module from Dev to Release

--- Doe.PVMapper/Scripts/pvMapper/TSMapper/DemModule.ts
+++ Doe.PVMapper/Scripts/pvMapper/TSMapper/DemModule.ts
@@ -13,8 +13,8 @@ module BYUModules {
                 author: "Darian Ramage, BYU",
                 version: "0.1.ts",
 
-                activate: () => { },
-                deactivate: null,
+                activate: () => { addMap(); },
+                deactivate: () => { removeMap(); },
                 destroy: null,
                 init: null,
 
@@ -24,11 +24,11 @@ module BYUModules {
                     destroy: null,
                     init: null,
 
-                    title: "Slope Tool",
+                    title: "Slope",
                     description: "Calculates the average slope of the site",
                     onScoreAdded: (event, score) => { },
-                    onSiteChange: (event, score: pvMapper.Score) => { },
-                    updateScoreCallback: (score: pvMapper.Score) => { },
+                    onSiteChange: (event, score: pvMapper.Score) => { updateScore(score, "any:3", "degrees"); }, //TODO: is this degrees?
+                    updateScoreCallback: (score: pvMapper.Score) => { updateScore(score, "any:3", "degrees"); }, // or maybe it's grade?
                 },
                 {
                     activate: null,
@@ -36,11 +36,13 @@ module BYUModules {
                     destroy: null,
                     init: null,
 
-                    title: "Aspect Tool",
+                    title: "Aspect",
                     description: "Calculates the average aspect of the site",
                     onScoreAdded: (event, score) => { },
-                    onSiteChange: (event, score: pvMapper.Score) => { },
-                    updateScoreCallback: (score: pvMapper.Score) => { },
+                    onSiteChange: (event, score: pvMapper.Score) => { updateScore(score, "any:4", "degrees"); }, //TODO: is this degrees?
+                    updateScoreCallback: (score: pvMapper.Score) => { updateScore(score, "any:4", "degrees"); }, // it's not radian.
+                    //TODO: should we translate the aspect score into a "degrees away from south" score, or something?
+                    //      I assume that south is the best...
                 },
                 {
                     activate: null,
@@ -48,18 +50,94 @@ module BYUModules {
                     destroy: null,
                     init: null,
 
-                    title: "Elevation Tool",
+                    title: "Elevation",
                     description: "Calculates the averate elevation of the site",
                     onScoreAdded: (event, score) => { },
-                    onSiteChange: (event, score: pvMapper.Score) => { },
-                    updateScoreCallback: (score: pvMapper.Score) => { },
-                }],
+                    onSiteChange: (event, score: pvMapper.Score) => { updateScore(score, "any:1", "m"); },
+                    updateScoreCallback: (score: pvMapper.Score) => { updateScore(score, "any:1", "m"); },
+                    //Note: I have no idea why, but the server will not find the correct layer if 
+                }
+                ],
                 infoTools: null
             });
         }
     }
 
     var modInstance = new DemModule();
-    var request = OpenLayers.Request.POST;
+
+    //All private functions and variables go here. They will be accessible only to this module because of the AEAF (Auto-Executing Anonomous Function)
+
+    var topoMapRestUrl = "http://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/";
+    var topoMapLayer;
+
+    function addMap() {
+        topoMapLayer = new OpenLayers.Layer.ArcGIS93Rest(
+            "World Topography",
+            topoMapRestUrl + "export",
+            {
+                layers: "visible", //"2",
+                format: "gif",
+                srs: "3857", //"102100",
+                //transparent: "true",
+            },
+            { isBaseLayer: true } //Note: this looks awful as an overlay - let's use it as a base layer, as nature intended
+        );
+        //topoMapLayer.setOpacity(0.3);
+        topoMapLayer.epsgOverride = "3857"; //"102100";
+        topoMapLayer.setVisibility(false);
+        pvMapper.map.addLayer(topoMapLayer);
+    }
+
+    function removeMap() {
+        pvMapper.map.removeLayer(topoMapLayer, false);
+    }
+
+    function updateScore(score: pvMapper.Score, layers: string, description?: string) {
+        var params = {
+            mapExtent: score.site.geometry.bounds.toBBOX(6, false),
+            geometryType: "esriGeometryEnvelope",
+            geometry: score.site.geometry.bounds.toBBOX(6, false),
+            f: "json",
+            layers: layers,
+            tolerance: 0,
+            imageDisplay: "1, 1, 96",
+            returnGeometry: false,
+        };
+
+        var request = OpenLayers.Request.GET({
+            url: topoMapRestUrl + "identify",
+            proxy: "/Proxy/proxy.ashx?",
+            params: params,
+            callback: (response) => {
+                // update value
+                if (response.status === 200) {
+                    var esriJsonPerser = new OpenLayers.Format.JSON();
+                    esriJsonPerser.extractAttributes = true;
+                    var parsedResponse = esriJsonPerser.read(response.responseText);
+
+                    if (parsedResponse && parsedResponse.results) {
+                        if (parsedResponse.results.length > 0) {
+                            console.assert(parsedResponse.results.length === 1,
+                                "I expected that the server would only return identify" +
+                                " results for the single pixel at the center of a site; boy, was I ever wrong.");
+
+                            score.popupMessage = parsedResponse.results[0].value + " " + description;
+                            score.updateValue(parseFloat(parsedResponse.results[0].value));
+                        } else {
+                            score.popupMessage = "No data for this site";
+                            score.updateValue(Number.NaN);
+                        }
+                    } else {
+                        score.popupMessage = "Parse error";
+                        score.updateValue(Number.NaN);
+                    }
+                } else {
+                    score.popupMessage = "Connection error " + response.status;
+                    score.updateValue(Number.NaN);
+                }
+            },
+        });
+    }
+
 }
 
--- Doe.PVMapper/Scripts/pvMapper/TSMapper/demModule.js
+++ Doe.PVMapper/Scripts/pvMapper/TSMapper/demModule.js
@@ -13,8 +13,11 @@ var BYUModules;
                 author: "Darian Ramage, BYU",
                 version: "0.1.ts",
                 activate: function () {
+                    addMap();
                 },
-                deactivate: null,
+                deactivate: function () {
+                    removeMap();
+                },
                 destroy: null,
                 init: null,
                 scoringTools: [
@@ -23,49 +26,126 @@ var BYUModules;
                         deactivate: null,
                         destroy: null,
                         init: null,
-                        title: "Slope Tool",
+                        title: "Slope",
                         description: "Calculates the average slope of the site",
                         onScoreAdded: function (event, score) {
                         },
                         onSiteChange: function (event, score) {
+                            updateScore(score, "any:3", "degrees");
                         },
-                        updateScoreCallback: function (score) {
+                        updateScoreCallback: //TODO: is this degrees?
+                        function (score) {
+                            updateScore(score, "any:3", "degrees");
                         }
                     }, 
+                    // or maybe it's grade?
                     {
                         activate: null,
                         deactivate: null,
                         destroy: null,
                         init: null,
-                        title: "Aspect Tool",
+                        title: "Aspect",
                         description: "Calculates the average aspect of the site",
                         onScoreAdded: function (event, score) {
                         },
                         onSiteChange: function (event, score) {
+                            updateScore(score, "any:4", "degrees");
                         },
-                        updateScoreCallback: function (score) {
+                        updateScoreCallback: //TODO: is this degrees?
+                        function (score) {
+                            updateScore(score, "any:4", "degrees");
                         }
                     }, 
+                    // it's not radian.
+                    //TODO: should we translate the aspect score into a "degrees away from south" score, or something?
+                    //      I assume that south is the best...
                     {
                         activate: null,
                         deactivate: null,
                         destroy: null,
                         init: null,
-                        title: "Elevation Tool",
+                        title: "Elevation",
                         description: "Calculates the averate elevation of the site",
                         onScoreAdded: function (event, score) {
                         },
                         onSiteChange: function (event, score) {
+                            updateScore(score, "any:1", "m");
                         },
                         updateScoreCallback: function (score) {
+                            updateScore(score, "any:1", "m");
                         }
                     }
                 ],
-                infoTools: null
+                infoTools: //Note: I have no idea why, but the server will not find the correct layer if
+                null
             });
         }
         return DemModule;
     })();    
     var modInstance = new DemModule();
-    var request = OpenLayers.Request.POST;
+    //All private functions and variables go here. They will be accessible only to this module because of the AEAF (Auto-Executing Anonomous Function)
+    var topoMapRestUrl = "http://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/";
+    var topoMapLayer;
+    function addMap() {
+        topoMapLayer = new OpenLayers.Layer.ArcGIS93Rest("World Topography", topoMapRestUrl + "export", {
+            layers: "visible",
+            format: //"2",
+            "gif",
+            srs: "3857"
+        }, //"102100",
+        //transparent: "true",
+        {
+            isBaseLayer: true
+        });
+        //Note: this looks awful as an overlay - let's use it as a base layer, as nature intended
+        //topoMapLayer.setOpacity(0.3);
+        topoMapLayer.epsgOverride = "3857"//"102100";
+        ;
+        topoMapLayer.setVisibility(false);
+        pvMapper.map.addLayer(topoMapLayer);
+    }
+    function removeMap() {
+        pvMapper.map.removeLayer(topoMapLayer, false);
+    }
+    function updateScore(score, layers, description) {
+        var params = {
+            mapExtent: score.site.geometry.bounds.toBBOX(6, false),
+            geometryType: "esriGeometryEnvelope",
+            geometry: score.site.geometry.bounds.toBBOX(6, false),
+            f: "json",
+            layers: layers,
+            tolerance: 0,
+            imageDisplay: "1, 1, 96",
+            returnGeometry: false
+        };
+        var request = OpenLayers.Request.GET({
+            url: topoMapRestUrl + "identify",
+            proxy: "/Proxy/proxy.ashx?",
+            params: params,
+            callback: function (response) {
+                // update value
+                if(response.status === 200) {
+                    var esriJsonPerser = new OpenLayers.Format.JSON();
+                    esriJsonPerser.extractAttributes = true;
+                    var parsedResponse = esriJsonPerser.read(response.responseText);
+                    if(parsedResponse && parsedResponse.results) {
+                        if(parsedResponse.results.length > 0) {
+                            console.assert(parsedResponse.results.length === 1, "I expected that the server would only return identify" + " results for the single pixel at the center of a site; boy, was I ever wrong.");
+                            score.popupMessage = parsedResponse.results[0].value + " " + description;
+                            score.updateValue(parseFloat(parsedResponse.results[0].value));
+                        } else {
+                            score.popupMessage = "No data for this site";
+                            score.updateValue(Number.NaN);
+                        }
+                    } else {
+                        score.popupMessage = "Parse error";
+                        score.updateValue(Number.NaN);
+                    }
+                } else {
+                    score.popupMessage = "Connection error " + response.status;
+                    score.updateValue(Number.NaN);
+                }
+            }
+        });
+    }
 })(BYUModules || (BYUModules = {}));
