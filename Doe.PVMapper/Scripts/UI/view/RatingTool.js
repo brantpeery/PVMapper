39c5130dbbeb2c091db6227f95d031b4b9b54b3f
LengVang_cp <LengVang@inl.gov>
2013-05-15 18:48:21 +0000

    debugged and tested the RatingTool class.

---
+++ Doe.PVMapper/Scripts/UI/view/RatingTool.js
@@ -0,0 +1,100 @@
+﻿
+Ext.define('RatingModel', {
+  extend: 'Ext.data.Model',
+  fields: [{
+    name: 'name'
+  }, {
+    name: 'value',
+    type: 'object'
+  }]
+});
+
+
+Ext.define('RatingOptionModel', {
+  extend: 'Ext.data.Model',
+  fields: [{
+    name: 'name',
+  }, {
+    name: 'value',
+    type: 'object'
+  }]
+});
+
+
+var handleItems = function (grid, rowIndex, colIndex, item, e, record) {
+  record.set('value', item.index);
+  record.raw.value = item.index;
+}
+
+Ext.define('MainApp.view.RatingTool', {
+  extend: 'Ext.grid.Panel',
+  columnLines: true,
+  width: 600,
+  height: 300,
+  title: 'Rate',
+  renderTo: Ext.getBody(),
+  onURL: 'http://www.iconshock.com/img_jpg/MODERN/general/jpg/16/star_icon.jpg',
+  offURL: 'http://www.iconshock.com/img_jpg/VECTORNIGHT/general/jpg/16/star_icon.jpg',
+  getData: function () {
+    return this.store.proxy.data;
+  },
+
+  /*
+  NOTE:  Any custom class that to be create/destroy and recreate must be wrapped inside 'initComponent' property.  -- leng.
+  */
+  initComponent: function() {
+    Ext.apply(this, {
+      listeners: {
+        beforerender: function (ts, eOpts) {
+          var me = this;
+          var aOption = {};
+          Ext.merge(aOption, me.options);
+          me.options = aOption;
+
+          me.columns[1].items = [];
+          var m = me.options.data.getCount();
+
+          for (i = 0; i < m; i++) {
+            me.columns[1].items.push({
+              index: i + 1,
+              tooltip: aOption.data.items[i].raw.name,
+              handler: handleItems,
+              icon: me.onURL
+            });
+          }
+          return true;
+        },
+        beforedestroy: function (ts, ops) {
+          this.options = null;
+          return true;
+        }
+      },
+      columns: [{
+        // text: "Name",
+        width: 150,
+        dataIndex: 'name'
+      }, {
+        xtype: 'actioncolumn',
+        //  text: 'Rating',
+        width: 150,
+        renderer: function (value, metadata, record) {
+          var me = this.ownerCt.ownerCt;
+          var n = record.get('value');
+
+          var m = me.options.data.getCount();
+          m = (m < n) ? n : m;
+
+          for (i = 0; i < n; i++) {
+            this.items[i].icon = me.onURL;
+          }
+
+          for (i = n; i < m; i++) {
+            this.items[i].icon = me.offURL;
+          }
+        },
+        items: []
+      }]
+    });
+    this.callParent(arguments);
+  }
+});
--- Doe.PVMapper/Scripts/UI/view/RatingView.js
+++ Doe.PVMapper/Scripts/UI/view/RatingView.js
@@ -1,174 +1,85 @@
-﻿
-Ext.define('RatingModel', {
-  extend: 'Ext.data.Model',
-  fields: [{
-    name: 'name'
-  }, {
-    name: 'value',
-    type: 'object'
-  }]
-});
+﻿Ext.require('Scripts.UI.view.RatingTool');
 
-
-Ext.define('RatingOptionModel', {
-  extend: 'Ext.data.Model',
-  fields: [{
-    name: 'name',
-  }, {
-    name: 'value',
-    type: 'object'
-  }]
-});
-
-
-var handleItems = function (grid, rowIndex, colIndex, item, e, record) {
-  record.set('value', item.index);
-  record.raw.value = item.index;
-}
-
-var ratingTool;
-
-Ext.define('MainApp.view.RatingTool', {
-  extend: 'Ext.grid.Panel',
-
-  store: null,
-  options: null,
-  onURL: 'http://www.iconshock.com/img_jpg/MODERN/general/jpg/16/star_icon.jpg',
-  offURL: 'http://www.iconshock.com/img_jpg/VECTORNIGHT/general/jpg/16/star_icon.jpg',
-  listeners: {
-    beforerender: function (ts, eOpts) {
-      ratingTool = this;
-      var aOption = {};
-      Ext.merge(aOption, ratingTool.options);
-      ratingTool.options = aOption;
-
-      ratingTool.columns[1].items = [];
-      var m = ratingTool.options.getCount();
-
-      for (i = 0; i < m; i++) {
-        ratingTool.columns[1].items.push({
-          index: i + 1,
-          tooltip: aOption.data.items[i].raw.name,
-          handler: handleItems,
-          icon: ratingTool.onURL
-        });
-      }
-      return true;
-    }
-  },
-  getData: function () {
-    return ratingTool.store.proxy.data;
-  },
-  columns: [{
-    // text: "Name",
-    width: 150,
-    dataIndex: 'name'
-  }, {
-    xtype: 'actioncolumn',
-    //  text: 'Rating',
-    width: 150,
-    renderer: function (value, metadata, record) {
-      var n = record.get('value');
-
-      var m = ratingTool.options.getCount();
-      m = (m < n) ? n : m;
-
-      for (i = 0; i < n; i++) {
-        this.items[i].icon = ratingTool.onURL;
-      }
-
-      for (i = n; i < m; i++) {
-        this.items[i].icon = ratingTool.offURL;
-      }
+var ratingStore = function () {
+  return Ext.create('Ext.data.Store', {
+    model: 'RatingModel',
+    data: ratingData = {
+      objects: [{
+        name: 'Foo',
+        value: 5
+      }, {
+        name: 'Bar',
+        value: 2
+      }, {
+        name: 'Gut Power',
+        value: 4
+      }, {
+        name: 'Instinct',
+        value: 5
+      }, {
+        name: 'E=MC^2',
+        value: 2
+      }, {
+        name: 'AI',
+        value: 5
+      }, {
+        name: 'Human Brain',
+        value: 3
+      }]
     },
-    items: []
-  }],
-  columnLines: true,
-  width: 600,
-  height: 300,
-  title: 'Rate',
-  renderTo: Ext.getBody()
-});
-
-var ratingData = {
-  objects: [{
-    name: 'Foo',
-    value: 3
-  }, {
-    name: 'Bar',
-    value: 2
-  }, {
-    name: 'Gut Power',
-    value: 4
-  }, {
-    name: 'Instinct',
-    value: 5
-  }, {
-    name: 'E=MC^2',
-    value: 2
-  }, {
-    name: 'AI',
-    value: 2
-  }, {
-    name: 'Human Brain',
-    value: 3
-  }]
+    autoLoad: true,
+    autoSync: true,
+    proxy: {
+      type: 'memory',
+      reader: {
+        type: 'json',
+        root: 'objects'
+      },
+    }
+  })
+};
+var optionStore = function () {
+  return Ext.create('Ext.data.Store', {
+    model: 'RatingOptionModel',
+    data: {
+      options: [{
+        name: 'worse',
+        value: 0
+      }, {
+        name: 'bad',
+        value: 1
+      }, {
+        name: 'fair',
+        value: 2
+      }, {
+        name: 'good',
+        value: 3
+      }, {
+        name: 'best',
+        value: 4
+      }, {
+        name: 'wonderful',
+        value: 5
+      }]
+    },
+    autoLoad: true,
+    autoSynch: true,
+    proxy: {
+      type: 'memory',
+      reader: {
+        type: 'json',
+        root: 'options'
+      }
+    }
+  })
 };
 
-var ratingStore = Ext.create('Ext.data.Store', {
-  model: 'RatingModel',
-  data: ratingData,
-  autoLoad: true,
-  autoSync: true,
-  proxy: {
-    type: 'memory',
-    reader: {
-      type: 'json',
-      root: 'objects'
-    },
-  }
-});
-
-var optionData = {
-  options: [{
-    name: 'worse',
-    value: 0
-  }, {
-    name: 'bad',
-    value: 1
-  }, {
-    name: 'fair',
-    value: 2
-  }, {
-    name: 'good',
-    value: 3
-  }, {
-    name: 'best',
-    value: 4
-  }, {
-    name: 'wonderful',
-    value: 5
-  }]
+var ratingPanel = function () {
+  return Ext.create('MainApp.view.RatingTool', {
+   store: ratingStore(),
+   options: optionStore()
+  })
 };
-
-var optionStore = Ext.create('Ext.data.Store', {
-  model: 'RatingOptionModel',
-  data: optionData,
-  autoLoad: true,
-  autoSynch: true,
-  proxy: {
-    type: 'memory',
-    reader: {
-      type: 'json',
-      root: 'options'
-    }
-  }
-});
-
-var ratingPanel = Ext.create('MainApp.view.RatingTool', {
-  store: ratingStore,
-  options: optionStore
-});
 
 
 Ext.define('MainApp.view.RatingView', {
@@ -176,19 +87,19 @@ Ext.define('MainApp.view.RatingView', {
   title: 'Rating',
   width: 400,
   modal: true,
-  layout: '',
-  overflowY: 'auto',
-  items: ratingPanel,
-  buttons: [{
-    xtype: 'button',
-    text: 'OK',
-    handler: function () { }
-  }, {
-    xtype: 'button',
-    text: 'Cancel',
-    handler: function () { }
-  }]
-  //constraint: true
+  constraint: true,
+  closeAction: 'destroy',
+  layout:'fit',
+  initComponent: function() {
+    Ext.apply(this, {
+      items: [Ext.create('MainApp.view.RatingTool', {
+        store: ratingStore(),
+        options: optionStore()
+      })
+      ]
+    });
+    this.callParent(arguments);
+  }
 });
 
 //var ratingView = Ext.create('RatingView').show();
