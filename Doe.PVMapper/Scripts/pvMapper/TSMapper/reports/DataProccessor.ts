95bf2b1cce9fd1ef5198062295377c71728f0ede
brantpeery_cp <brant@atechinnovations.com>
2013-11-06 08:55:42 +0000

    Add a dataprocessor script and a reports directory. Updated the application index file to include the new dataprocessor script

---
+++ Doe.PVMapper/Scripts/pvMapper/TSMapper/reports/DataProccessor.ts
@@ -0,0 +1,39 @@
+﻿/// <reference path="../Scoreboard.ts" />
+/// <reference path="../ScoreLine.ts" />
+
+
+module pvMapper {
+   export module Data {
+        export class ScoreboardProcessor {
+            constructor(scoreboard: ScoreBoard) {
+                this.scoreboard = scoreboard;
+            }
+            private scoreboard: ScoreBoard;
+            public getCleanObject() {
+                var j = JSON.stringify(this.scoreboard);
+                return JSON.parse(j);
+            }
+            public getCleanObjectTransposed() {
+                var obj = this.getCleanObject();
+                var newObj = {
+                    sites: []
+                };
+                obj.scoreLines.forEach(function (line, sidx) {
+                    line.scores.forEach(function (score, idx) {
+                        if (newObj.sites[idx] == undefined) {
+                            newObj.sites[idx] = score.site;
+                            newObj.sites[idx].scores = []
+                            }
+                        newObj.sites[idx].scores[sidx] = score;
+                        newObj.sites[idx].scores[sidx].scoreLine = line;
+                        delete score.site;
+                    });
+                    delete line.scores;
+                });
+                return newObj;
+            }
+            public toJSON() { }
+            public fromJSON() { }
+        }    
+    }
+}
